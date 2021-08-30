78f978cb73e0089110149c89b81923bfc8501ce6
praveena1212 <praveena9788@gmail.com>
2021-08-30 12:56:40 +0530

    my task is done

---
+++ src/org/tcs/project/ProjectDataTypes.java
@@ -0,0 +1,18 @@
+package org.tcs.project;
+
+import java.util.Scanner;
+
+public class ProjectDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProjectDtaTypes s1 = new ProjectDataTypes(system.in);
+		system.out.println("please enter the byte value");
+	     byte b = s1.nextByte();
+				
+	   
+	     
+
+	}
+
+}
---
+++ src/org/tcs/project/ProjectDtaTypes.java
@@ -0,0 +1,11 @@
+package org.tcs.project;
+
+public class ProjectDtaTypes {
+     public void nextByte() {
+    	 System.out.println("nextByte"+b);
+     }
+
+
+	}
+
+
